b5723db63e289624687c72770390f771fdd4b69d
Gregoire Vallette <gregoire.vallette@gmail.com>
2021-12-10 11:26:53 +0100

    navigation, arrows

--- scripts/modules/swipe.js
+++ scripts/modules/swipe.js
@@ -5,6 +5,39 @@ let left = current.querySelector('.left').value
 let up = current.querySelector('.up').value
 let down = current.querySelector('.down').value
 
+const updateArrows = () => {
+  if (right != '') {
+    document.getElementById('box-right').style.opacity = ('0.8');
+    document.getElementById('box-right').classList.add('blink');
+  } else {
+    document.getElementById('box-right').style.opacity = ('0');
+    document.getElementById('box-right').classList.remove('blink');
+  }
+  if (left != '') {
+    document.getElementById('box-left').style.opacity = ('0.8');
+    document.getElementById('box-left').classList.add('blink');
+  } else {
+    document.getElementById('box-left').style.opacity = ('0');
+    document.getElementById('box-left').classList.remove('blink');
+  }
+  if (up != '') {
+    document.getElementById('box-up').style.opacity = ('0.8');
+    document.getElementById('box-up').classList.add('blink');
+  } else {
+    document.getElementById('box-up').style.opacity = ('0');
+    document.getElementById('box-up').classList.remove('blink');
+  }
+  if (down != '') {
+    document.getElementById('box-down').style.opacity = ('0.8');
+    document.getElementById('box-down').classList.add('blink');
+  } else {
+    document.getElementById('box-down').style.opacity = ('0');
+    document.getElementById('box-down').classList.remove('blink');
+  }
+}
+
+updateArrows();
+
 const reset = () => {
   next.classList.remove('set-left');
   next.classList.remove('set-right');
@@ -17,11 +50,12 @@ const reset = () => {
   next.classList.remove('move-down');
 
   current = document.querySelector('.shown');
-
   right = current.querySelector('.right').value;
   left = current.querySelector('.left').value;
   up = current.querySelector('.up').value;
   down = current.querySelector('.down').value;
+
+  updateArrows();
 }
 
 const swipeRight = () => {
@@ -35,6 +69,7 @@ const swipeRight = () => {
     next.classList.remove('hidden');
     next.classList.add('shown');
     next.classList.add('move-left');
+
     setTimeout(() => {
       reset();
     }, 500);
@@ -52,6 +87,7 @@ const swipeLeft = () => {
     next.classList.remove('hidden');
     next.classList.add('shown');
     next.classList.add('move-right');
+
     setTimeout(() => {
       reset();
     }, 500);
@@ -69,6 +105,7 @@ const swipeUp = () => {
     next.classList.remove('hidden');
     next.classList.add('shown');
     next.classList.add('move-down');
+
     setTimeout(() => {
       reset();
     }, 500);
@@ -86,6 +123,7 @@ const swipeDown = () => {
     next.classList.remove('hidden');
     next.classList.add('shown');
     next.classList.add('move-up');
+
     setTimeout(() => {
       reset();
     }, 500);
