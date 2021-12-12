dcf0b0578c0504879c4c4d3b81be5a0a72b1148e
Gregoire Vallette <gregoire.vallette@gmail.com>
2021-12-12 15:25:07 +0100

    trying to fix the vh problem on mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
 import { listeners } from './modules/listeners.js'
 
-const outer = document.querySelector('.outer')
+// const outer = document.querySelector('.outer')
 
-window.onload = () => {
-  outer.style.width = `${window.innerWidth}px`;
-  outer.style.height = `${window.innerHeight}px`;
-}
+// window.onload = () => {
+//   outer.style.width = `${window.innerWidth}px`;
+//   outer.style.height = `${window.innerHeight}px`;
+// }
 
-window.onresize = () => {
-  outer.style.width = `${window.innerWidth}px`;
-  outer.style.height = `${window.innerHeight}px`;
-}
+// window.onresize = () => {
+//   outer.style.width = `${window.innerWidth}px`;
+//   outer.style.height = `${window.innerHeight}px`;
+// }
 
 listeners();
